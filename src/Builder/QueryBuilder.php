72df4273437865b532ae6ceb88765b12545504a8
Kevin Giguère <kgiguere@o2web.ca>
2015-02-25 11:56:14 -0500

    remove debug

--- src/Builder/QueryBuilder.php
+++ src/Builder/QueryBuilder.php
@@ -163,7 +163,6 @@ class QueryBuilder {
 			
 			
 		}else{
-			kpr($filters);
 			foreach ($filters as $key => $parameter) {
 				$condition = null;
 				if (in_array((string)$key, array(static::ES_FIELD_MUST, static::ES_FIELD_MUST_NOT, static::ES_FIELD_SHOULD))) {
@@ -184,7 +183,6 @@ class QueryBuilder {
 						}
 						$filterStragety = $this->getFilterStrategy($strategy);
 						$filterStragety->updateFromArray($entry);
-						kpr(array($filterStragety,$condition));
 						$this->preparedParams = $this->addFilter($filterStragety->getFilter(), $condition);
 					}
 				}
